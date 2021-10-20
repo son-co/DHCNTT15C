9b2f95011c1bfb3af2698604f18902d2c4ce387b
son-co <82647500+son-co@users.noreply.github.com>
2021-10-20 14:48:33 +0700

    Update UDP datetime

--- Tuan4/Tuan4/UDPDateTimeClient.java
+++ Tuan4/Tuan4/UDPDateTimeClient.java
@@ -1,58 +1,60 @@
 package Tuan4;
 
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.io.InputStreamReader;
+
+
+
 
 public class UDPDateTimeClient {
-    public static void main(String[] args) throws Exception {
-        //Tạo socket cho client
-        DatagramSocket clientSocket = new DatagramSocket(6789); 
-        
-        //Tạo địa chỉ IP address bỏi tên "localhost)
-        InetAddress IPAddress = InetAddress.getByName("localhost"); 
-        System.out.println("Client da duoc tao!");
+    public static void main(String[] args) {
+        try {
+            DatagramSocket cs=new DatagramSocket();
 
-        while (true) {
-            //Tạo dữ liệu(group of bytes) cho gói tin nhận và gói tin gủi
-            byte[] sendData = new byte[1024]; 
-            byte[] receiveData = new byte[2048];
+            System.out.println("Client da dc tao!");
+
+            while (true) {
 
 
-            String i;
-            do{
                 System.out.println("\n\t1.Date | 2.Time | 3. DateTime | 0.Exit\n");
                 System.out.println("\nNhap lua chon cua ban: ");
-                           
-                //lấy dòng văn bản nhập từ bàn phím và gán cho biến sentence
-                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
-                 i = inFromUser.readLine();
-           
-                //tạo dữ liệu gửi với dữ liệu vừa nhập từ bàn phím
-                sendData = i.getBytes();
-                //Tạo gói tin để truyền đi
-                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
-                //Truyền đến server bằng phương thức send()
-                clientSocket.send(sendPacket); 
-                               
 
-                 //tạo gói tin nhận
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
-                //lấy biến receivePacket để nhận gói tin bằng phương thức receive()
-                clientSocket.receive(receivePacket); 
+                BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+                String bString = read.readLine();
 
-                //Chuyển dữu liệu trong gói tin vừa nhận thành kiểu String và in ra
-                String modified_Sentence = new String(receivePacket.getData());
-                System.out.println( modified_Sentence); 
+                byte[] ghi = bString.getBytes();
+
+                DatagramPacket in = new DatagramPacket(ghi, ghi.length,InetAddress.getByName("localhost"),9876);
+
+                cs.send(in);
+
+                byte[] nhan = new byte[1024];
+
+                DatagramPacket os = new DatagramPacket(nhan, nhan.length);
+                cs.receive(os);;
+
+                String doi = new String(os.getData(),0,os.getLength());
+                System.out.println(doi);
+
+                if(bString.compareTo("0")==0){
+                    System.out.println("Client da dong!");
+                    cs.close();
+                    
+                    break;
+                }
+                
+            }
+            
 
 
-            }while(i != null);
 
- 
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
         }
-        
-        
     }
+
 }
--- Tuan4/Tuan4/UDPDateTimeServer.java
+++ Tuan4/Tuan4/UDPDateTimeServer.java
@@ -3,53 +3,62 @@ package Tuan4;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 public class UDPDateTimeServer {
-    public static void main(String[] args) throws Exception {
-        //Tạo socket cho server
-        DatagramSocket serverSocket = new DatagramSocket(9876);
+    public static void main(String[] args) {
+        try {
+            DatagramSocket ds = new DatagramSocket(9876);
 
-        while (true) {
-            //tạo biến nhận dữ liệu 
-            byte[] receiver = new byte[1024];
-            //tạo biến gửi dữ liệu
-            byte[] send = new byte[2048];
             System.out.println("Server da duoc tao!");
 
-             //tạo biến receivePacket để nhận gói tin từ socket
-             DatagramPacket receivePacket = new DatagramPacket(receiver, receiver.length); 
-            //nhận gói tin qua phương thức receive()
-            serverSocket.receive(receivePacket);
-            
-            
+            byte[] give = new byte[1024];
+            while (true) {
+                DatagramPacket in = new DatagramPacket(give, give.length);
+                ds.receive(in);
 
-             //Lấy địa chỉ IP của bên gửi
-            InetAddress IPAddress = receivePacket.getAddress();
-            //Lấy số hiệu cổng bên gửi
-            int port = receivePacket.getPort(); 
-            DatagramPacket sendPacket = new DatagramPacket(send, send.length, IPAddress, port);
-            //Chuyển dữ liệu vừa nhận về dạng String
-            String sentence = new String(receivePacket.getData()); 
+                String bString = new String(in.getData(),0,in.getLength());
+                System.out.println("Client: "+bString);
 
-            switch (sentence) {
-                case "1":
-                    String date= "Ngay hien tai : "+ java.time.LocalTime.now();
-                    send= date.getBytes();
-                    //Gửi gói tin đi
-                    serverSocket.send(sendPacket);
-                    break;
-            
-                default:
-                    break;
+                byte[] send = new byte[1024];
+                DatagramPacket os;
+
+                switch (bString) {
+
+                    case "1":
+                        String date= "Ngay hien tai: "+ java.time.LocalDate.now();
+                        send=date.getBytes();
+                        os = new DatagramPacket(send, send.length, in.getAddress(), in.getPort());
+                        ds.send(os);
+                        break;
+                    case "2": 
+                        String time= "Gio hien tai: "+ java.time.LocalTime.now();
+                        send=time.getBytes();
+
+                        os = new DatagramPacket(send, send.length, in.getAddress(), in.getPort());
+
+                        ds.send(os);
+                        break;
+                    case "3":
+                        String datetime= "Ngay gio hien tai: "+ java.time.LocalDateTime.now();
+                        send=datetime.getBytes();
+                        os = new DatagramPacket(send, send.length, in.getAddress(), in.getPort());
+
+                        ds.send(os);
+                        break;
+                    case "0":
+                        System.exit(0);
+                        break;
+                
+                    default:
+                        System.exit(0);
+                        break;
+                }
             }
 
-            
-            
-
-            
+        } catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
         }
-
-
     }
+
 }
